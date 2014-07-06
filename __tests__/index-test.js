104c10a1beb13235d1f9347a688cd61d89a3e647
Cheng Lou <chenglou92@gmail.com>
2014-07-05 17:35:39 -0700

    Update tests for the new API

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -3,58 +3,104 @@ jest
   .dontMock('../styleRuleConverter')
   .dontMock('../styleRuleValidator');
 
-var RCSS = require('../');
+var RCSS;
+var classNamePattern = /c0-\w+/;
+var classNameSuffixPattern = /.+-(.+$)/;
 
 describe('RCSS', function() {
-  it('should have injected an empty style tag into the head', function() {
-    expect(document.querySelectorAll('style').length).toBe(1);
-    expect(document.querySelector('style').innerHTML).toBe('');
+  beforeEach(function() {
+    RCSS = require('../');
   });
 
-  describe('merge', function() {
+  describe('cascade', function() {
     it('should merge objects by overriding the left one', function() {
       var a = {a: 5};
       var b = {a: 6, b: 7};
 
-      expect(RCSS.merge(a, b)).toEqual({a: 6, b: 7});
+      expect(RCSS.cascade(a, b)).toEqual({a: 6, b: 7});
+    });
+
+    it('should deeply merge', function() {
+      // In our case, we only need to deeply merge objects.
+      var a = {a: {c: 5, d: 6}};
+      var b = {a: {c: 7, e: 8}, b: 9};
+
+      expect(RCSS.cascade(a, b)).toEqual({a: {c: 7, d: 6, e: 8}, b: 9});
     });
 
     it('should not mutate anything', function() {
       var a = {a: 5};
       var b = {a: 6, b: 7};
-      var c = RCSS.merge(a, b);
+      var c = RCSS.cascade(a, b);
 
       expect(a).toEqual({a: 5});
-      RCSS.merge(a, {bla: 8});
+      RCSS.cascade(a, {bla: 8});
       expect(c).toEqual({a: 6, b: 7});
     });
   });
 
-  describe('createClass', function() {
-    var obj = {
-      display: 'inline',
-      padding: '6px 12px',
-      marginBottom: '0',
-      fontSize: '14px'
-    };
-
+  describe('registerClass', function() {
     it('should turn an object into a css class descriptor', function() {
-      var obj2 = RCSS.createClass(obj);
-      expect(obj2.display).toBe('inline');
-      expect(obj2.fontSize).toBe('14px');
-      expect(obj2.padding).toBe('6px 12px');
-      expect(obj2.marginBottom).toBe('0');
-      expect(obj2.className).toMatch(/c0-\w+/);
-      // expect(document.querySelector('style').innerHTML).toBe(
-      //   '.c0{display:inline;padding:6px 12px;margin-bottom:0;font-size:14px;}'
-      // );
+      var obj = {
+        display: 'inline',
+        padding: '6px 12px',
+        marginBottom: '0',
+        fontSize: '14px'
+      };
+      var obj2 = RCSS.registerClass(obj);
+      expect(obj2.style).toBe(obj);
+      expect(obj2.className).toMatch(classNamePattern);
     });
 
-    xit('should turn an object into a css class descriptor', function() {
-      // expect(RCSS.createClass(obj));
-      // expect(document.querySelector('style').innerHTML).toBe(
-      //   '.c0{display:inline;padding:6px 12px;margin-bottom:0;font-size:14px;}'
-      // );
+    it('should generate the same random class suffix every time', function() {
+      var className1 = RCSS.registerClass({display: 'none'}).className;
+      var className2 = RCSS.registerClass({display: 'block'}).className;
+      expect(className1.match(classNameSuffixPattern)[1])
+        .toBe(className2.match(classNameSuffixPattern)[1]);
     });
   });
-})
+
+  describe('getStylesString', function() {
+    it('has the side-effect of clearing the registered classes', function() {
+      expect(RCSS.getStylesString()).toBe('');
+      RCSS.registerClass({display: 'none'});
+      RCSS.registerClass({display: 'block'});
+      expect(RCSS.getStylesString()).not.toBe('');
+      expect(RCSS.getStylesString()).toBe('');
+    });
+
+    it('returns the string of style markup', function() {
+      var className = RCSS.registerClass({display: 'none'}).className;
+      RCSS.registerClass({display: 'block'});
+
+      var suffix = className.match(classNameSuffixPattern)[1];
+      expect(RCSS.getStylesString()).toBe(
+        '.c0-' + suffix + '{display:none;}.c1-' + suffix + '{display:block;}'
+      );
+    });
+  });
+
+  describe('injectAll', function() {
+    afterEach(function() {
+      // Clear the DOM.
+      [].slice.apply(document.querySelectorAll('style')).forEach(function(tag) {
+        tag.parentNode.removeChild(tag);
+      });
+    });
+
+    it('should not inject the style tag unless called', function() {
+      expect(document.querySelectorAll('style').length).toBe(0);
+      RCSS.injectAll();
+      expect(document.querySelectorAll('style').length).toBe(1);
+    });
+
+    it('should inject the markup', function() {
+      var suffix = RCSS.registerClass({display: 'none'}).className
+        .match(classNameSuffixPattern)[1];
+      RCSS.injectAll();
+      expect(document.querySelector('style').innerHTML).toBe(
+        '.c0-' + suffix + '{display:none;}'
+      );
+    });
+  });
+});
--- __tests__/styleRuleConverter-test.js
+++ __tests__/styleRuleConverter-test.js
@@ -1,16 +1,103 @@
-jest.dontMock('../styleRuleConverter');
+jest
+  .dontMock('../styleRuleConverter')
+  .dontMock('../styleRuleValidator');
 
-var styleRuleConverter = require('../styleRuleConverter');
+var styleRuleConverter;
+var r;
 
-describe('styleRuleConverter', function() {
-  it('should escape the style values correctly', function() {
-    expect(styleRuleConverter.escapeValueForProp('<>"\'&hello')).toBe(
-      '&lt;&gt;&quot;&#39;&amp;hello'
+describe('rulesToString', function() {
+  beforeEach(function() {
+    styleRuleConverter = require('../styleRuleConverter');
+    r = styleRuleConverter.rulesToString.bind(null, 'a');
+  });
+
+  describe('on normal styles', function() {
+    it('should construct many rules and concat them', function() {
+      expect(r({display: 'none', height: '10px'}))
+        .toBe('.a{display:none;height:10px;}');
+    });
+
+    it('should hyphenate', function() {
+      expect(r({backgroundColor: 'blue'})).toBe('.a{background-color:blue;}');
+    });
+
+    it('should ignore unknown props', function() {
+      var msg = '%s (transformed into %s) is not a valid CSS property name.';
+      spyOn(console, 'warn');
+
+      expect(r({foo: 'a', bar: 'b'})).toBe('');
+      expect(console.warn.argsForCall.length).toBe(2);
+      expect(console.warn.argsForCall[0][0]).toBe(msg);
+      expect(console.warn.argsForCall[1][0]).toBe(msg);
+    });
+
+    it('should accept correct vendor prefixes', function() {
+      expect(r({WebkitTransition: 'a', MozTransition: 'b'}))
+        .toBe('.a{-webkit-transition:a;-moz-transition:b;}');
+      expect(r({WebkitBla: 'a'})).toBe('');
+      expect(r({MsTransition: 'a'})).toBe('.a{-ms-transition:a;}');
+      // See note in styleRuleConverter#hyphenateProp
+      expect(r({msTransition: 'a'})).toBe('.a{-ms-transition:a;}');
+    });
+
+    it('should quote content', function() {
+      expect(r({content: 'bla'})).toBe('.a{content:"bla";}');
+    });
+
+    it('should not accept null values', function() {
+      expect(r({border: null, width: '', height: false})).toBe('');
+    });
+
+    it('should escape values', function() {
+      expect(r({border: '<>"\'&hello'}))
+        .toBe('.a{border:&lt;&gt;&quot;&#39;&amp;hello;}');
+      expect(r({content: '<>"\'&hello'}))
+        .toBe('.a{content:"<>"\'&hello";}');
+    });
+
+    it('should not accept null values', function() {
+      expect(r({border: null, width: '', height: false})).toBe('');
+    });
+
+    it('returns an empty string if everything was invalid', function() {
+      expect(r({foo: 'bar', display: false})).toBe('');
+    });
+  });
+
+  describe('on pseudo-selectors', function() {
+    it('parses pseudo-selectors', function() {
+      var style = {
+        border: 'none',
+        ':hover': {
+          border: '1px solid black'
+        }
+      };
+      expect(r(style)).toBe(
+        '.a{border:none;}.a:hover{border:1px solid black;}'
+      );
+    });
+
+    it('does not include an empty rule if no normal rule is specified',
+      function() {
+        expect(r({':before': {color: 'blue'}})).toBe('.a:before{color:blue;}');
+      }
     );
+
+    it('parses media-queries', function() {
+      var style = {
+        display: 'none',
+        '@media (max-width: 500px)': {
+          display: 'block'
+        },
+        '@media (min-width: 100px) and (max-width: 200px)': {
+          color: 'blue'
+        }
+      };
+      expect(r(style)).toBe(
+        '.a{display:none;}@media (max-width: 500px){.a{display:block;}}' +
+        '@media (min-width: 100px) and (max-width: 200px){.a{color:blue;}}'
+      );
+    });
   });
 
-  it('should hyphenate camelCases correctly', function() {
-    expect(styleRuleConverter.hyphenateProp('HelloWorld')).toBe('-hello-world');
-    expect(styleRuleConverter.hyphenateProp('hello-Ha')).toBe('hello--ha');
-  });
 });
