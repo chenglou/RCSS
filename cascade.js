7d3182b9325df94b2312d7417c13001e79a325fc
Raynos <raynos2@gmail.com>
2014-08-31 15:51:21 -0700

    use lighter dependencies

--- cascade.js
+++ cascade.js
@@ -1,9 +1,14 @@
-var merge = require('lodash.merge');
+var DeepMerge = require('deep-merge/multiple');
+var merge = DeepMerge(mergeStrategy);
 
 // currently `cascade` does a simple merge. In the future, we might have smarter
 // logic here for perf reasons.
 function cascade() {
-  return merge.bind(null, {}).apply(null, arguments);
+  return merge([].slice.call(arguments));
 }
 
 module.exports = cascade;
+
+function mergeStrategy(a, b) {
+    return b;
+}
