7b77538c3d9bca88b01fc59cdc51766eed76f59b
Charles Marsh <charlie@khanacademy.org>
2014-10-01 11:08:08 -0400

    Use deep-extend

--- cascade.js
+++ cascade.js
@@ -1,14 +1,9 @@
-var DeepMerge = require('deep-merge/multiple');
-var merge = DeepMerge(mergeStrategy);
-
-function mergeStrategy(a, b) {
-  return b;
-}
+var deepExtend = require('deep-extend');
 
 // currently `cascade` does a simple merge. In the future, we might have smarter
 // logic here for perf reasons.
 function cascade() {
-  return merge([].slice.call(arguments));
+  return deepExtend.bind(null, {}).apply(null, arguments);
 }
 
 module.exports = cascade;
